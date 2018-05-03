a60721d32fb23d4e0ed011f23c2bc6ccaac0ccd9
Damian Dziaduch <damian@dziaduch.pl>
2018-05-03 09:33:19 +0200

    Search is now working, yeey :D

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-const {app, BrowserWindow, Menu, MenuItem} = require('electron');
-const path = require('path');
-const url = require('url');
+const {app, BrowserWindow, Menu, MenuItem} = require('electron')
 
-let win;
-
-function createWindow() {
-  win = new BrowserWindow({ width: 1024, height: 768 });
-
-  win.loadURL('https://devdocs.io');
-
-  win.on('closed', () => {
-    win = undefined;
-  });
-
-  const menu = new Menu();
-  menu.append(new MenuItem({
-    label: 'Find',
-    accelerator: 'Cmd+F',
-    click: () => { document.querySelector('input[name=q]').focus(); }
-  }));
+function createMenu(window) {
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Find',
+          accelerator: 'CmdOrCtrl+F',
+          click () {
+            window.webContents.executeJavaScript(`document.querySelector('input[name=q]').focus()`)
+          },
+        },
+      ],
+    },
+  ]
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 }
 
-app.on('ready', createWindow);
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1024,
+    height: 768,
+  })
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  window.loadURL('https://devdocs.io')
+  createMenu(window);
+}
 
-app.on('activate', () => {
-  if (!win) {
-    createWindow();
-  }
-});
+app.on('ready', createWindow)
